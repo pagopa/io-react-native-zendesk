af958b2454fd2c5d789ce4d164c2dc3fe08d10ed
Cristiano Tofani <cri.tofani@gmail.com>
2024-01-29 10:38:09 +0100

    fix: module name remapped

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,4 @@
-declare module 'io-react-native-zendesk' {
-
+declare module "@pagppa/io-react-native-zendesk" {
   // function to display chat box
   export function startChat(chatOptions: ChatOptions): void;
 
@@ -19,10 +18,10 @@ declare module 'io-react-native-zendesk' {
   export function addTicketCustomField(key: string, value: string): void;
 
   // function to append a new line to the ticket log
-  export function appendLog(log: string)
+  export function appendLog(log: string);
 
   // add a new tag to the ticket
-  export function addTicketTag(tag: string)
+  export function addTicketTag(tag: string);
 
   // remove custom fields
   export function resetCustomFields(): void;
@@ -54,55 +53,56 @@ declare module 'io-react-native-zendesk' {
   // function to register notifications token with zendesk
   export function setNotificationToken(token: string): void;
 
-  export function setUserIdentity(identity: JwtIdentity | AnonymousIdentity): void;
+  export function setUserIdentity(
+    identity: JwtIdentity | AnonymousIdentity
+  ): void;
 
   export function resetUserIdentity(): void;
 
   interface ChatOptions extends UserInfo {
-    botName?: string
+    botName?: string;
     // boolean value if you want just chat sdk or want to use all the sdk like support, answer bot and chat
     // true value means just chat sdk
-    chatOnly?: boolean
+    chatOnly?: boolean;
     // hex code color to set on chat
-    color?: string
+    color?: string;
     /* help center specific props only */
     // sent in help center function only to show help center with/without chat
-    withChat?: boolean
+    withChat?: boolean;
     // to enable/disable ticket creation in help center
-    disableTicketCreation?: boolean
+    disableTicketCreation?: boolean;
   }
 
   interface InitOptions {
     // chat key of zendesk account to init chat
-    key: string,
+    key: string;
     // appId of your zendesk account
-    appId: string,
+    appId: string;
     // clientId of your zendesk account
-    clientId: string,
+    clientId: string;
     // support url of zendesk account
-    url: string,
+    url: string;
     // id of the log custom field
-    logId: string
+    logId: string;
   }
 
-  interface UserInfo extends AnonymousIdentity{
+  interface UserInfo extends AnonymousIdentity {
     // user's phone
-    phone?: number
+    phone?: number;
     // department to redirect the chat
-    department?: string
+    department?: string;
     // tags for chat
-    tags?: Array<string>
+    tags?: Array<string>;
   }
 
   interface JwtIdentity {
-    token: string
+    token: string;
   }
 
   interface AnonymousIdentity {
     // user's name
-    name?: string
+    name?: string;
     // user's email
-    email?: string
+    email?: string;
   }
-
 }
